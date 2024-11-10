525421c1b41f58bdfe268b7e7fc3a34cd4e83132
HumbertoHSC <hhsc.academico@gmail.com>
2024-11-10 16:45:11 -0300

    Implementa CRUD do clube: cadastro, atualizações e exclusão

--- src/rotas.js
+++ src/rotas.js
@@ -5,7 +5,9 @@ const autenticacao = require('./intermediarios/autenticacao')
 const validarCorpoReq = require('./intermediarios/validarCorpoReq')
 
 const schemaUser = require('./validacoes/usuario/schemaUser')
+const schemaClube = require('./validacoes/clube/schemaClube')
 const userController = require('./controladores/usuario/userControladores')
+const clubeControladores = require('./controladores/clube/clubeControladores')
 
 rotas.post('/cadastro', validarCorpoReq(schemaUser.cadastro), userController.cadastro)
 rotas.post('/login', validarCorpoReq(schemaUser.login), userController.login)
@@ -17,12 +19,6 @@ rotas.patch('/nome', validarCorpoReq(schemaUser.nome), userController.nome)
 rotas.patch('/senha', validarCorpoReq(schemaUser.senha), userController.senha)
 
 rotas.delete('/usuario', validarCorpoReq(schemaUser.deletar), userController.deletar)
-
-module.exports = rotas
-const validarCorpoReq = require('./intermediarios/validarCorpoReq')
-
-const schemaClube = require('./validacoes/clube/schemaClube')
-const clubeControladores = require('./controladores/clube/clubeControladores')
 
 rotas.post('/clubes', validarCorpoReq(schemaClube.criar), clubeControladores.criar)
 
